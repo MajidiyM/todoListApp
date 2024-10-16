e54897afd3e35b47870097f8c78e7c2d39917226
Mirislom <m.mirislom.m@gmail.com>
2024-10-16 17:49:50 +0500

    [+] BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_app/screens/detailed_task_screen/detailed_screen.dart';
+import 'package:todo_list_app/screens/settings_screen/settings.dart';
 import 'package:todo_list_app/screens/tasks_screen/tasks_screen.dart';
 
 import 'ui/theme/theme.dart';
@@ -13,8 +15,73 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "To-Do List",
-      home: TasksScreen(),
       theme: darkTheme,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: PageView(
+        children: [
+          TasksScreen(),
+          DetailedTaskScreen(),
+          SettingsScreen(),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: AddTask(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddTask extends StatelessWidget {
+  const AddTask({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      height: 64,
+      width: 64,
+      child: FloatingActionButton(
+        backgroundColor: theme.floatingActionButtonTheme.backgroundColor,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Icon(
+          Icons.add,
+          size: 30,
+        ),
+        onPressed: () {},
+      ),
     );
   }
 }
